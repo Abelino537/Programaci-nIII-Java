dffee1316f397eddf583e75a0eb25671387db54c
Abel Pierna <103205401+Abelino537@users.noreply.github.com>
2023-06-27 06:41:32 -0300

    Clase 8
    Agrego las carpetas con la clase 8 de java

---
+++ Semestre 3/S3_Clase8/src/Parte1/Persona.java
@@ -0,0 +1,47 @@
+/*
+Laboratorio III
+Abel Pierna
+Clase 7
+31/05/23
+Java Beans
+ */
+package Parte1;
+
+import java.io.Serializable;
+
+public class Persona implements Serializable{
+    private String nombre;
+    private String Apellido;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String Apellido) {
+        this.nombre = nombre;
+        this.Apellido = Apellido;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return this.Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", Apellido=" + Apellido + '}';
+    }
+    
+    
+    
+}
---
+++ Semestre 3/S3_Clase8/src/Parte2/ManejoDeExcepciones1.java
@@ -0,0 +1,12 @@
+/*
+Laboratorio III
+Abel Pierna
+Clase 7
+31/05/23
+Excepciones
+ */
+package Parte2;
+
+public class ManejoDeExcepciones1 {
+    
+}
